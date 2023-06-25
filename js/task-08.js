56e6438287420553cddd9bfc38f08edb306d17fa
user <user@MacBook-Pro-user.local>
2023-06-25 15:03:15 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
 
+const formEL = document.querySelector('.login-form');
+console.log(formEL);
+formEL.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const currentValue = event.currentTarget.elements;
+
+  if (currentValue.email.value === '' || currentValue.password.value === '') {
+    alert('Все поля должны быть заполнены.')
+  }
+
+  const email = currentValue.email.value;
+  const password = currentValue.password.value;
+
+  const formData = {
+    email,
+    password
+  }
+  console.log(formData);
+  event.currentTarget.reset();
+}
